605fa16ac5d82c0af871f1e14f0bb4ad7bacdb35
atilajp@gmail.com <Git@qaz123>
2021-01-27 14:23:38 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100'),
+                subtitle: Text('1000'),
+
+              )
+            ),            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100'),
+                subtitle: Text('1000'),
+
+              )
+            ),
+          ],
+        ),
+        appBar: AppBar(
+          title: Text('Trasferencias'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
+      ),
+    ));
